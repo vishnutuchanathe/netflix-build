c00ec263bc14807775c7431a0cf4d771e0066ab8
Vishnu S Mani <63743748+vishnutuchanathe@users.noreply.github.com>
2021-07-24 18:45:56 +0530

    Create landing page

---
+++ src/Nav.js
@@ -0,0 +1,28 @@
+import React, {useState, useEffect} from 'react'
+import './Nav.css'
+
+function Nav() {
+    const [show, handleShow] = useState(false);
+
+    const transitionNavBar = () => {
+        if(window.scrollY > 100) {
+            handleShow(true);
+        }else{
+            handleShow(false);
+        }
+    }
+    useEffect(() => {
+        window.addEventListener("scroll",transitionNavBar);
+        return () => window.removeEventListener("scroll", transitionNavBar);
+    }, []);
+    return (
+        <div className={`nav ${show && 'nav__black'}`}>
+        <div className="nav__contents">
+        <img className="nav__logo" src="logo.png" alt="" />
+        <img className="nav__avatar" src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="" />
+        </div>
+        </div>
+    )
+}
+
+export default Nav
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBgetl4iBcfMKBDsSOhKYHovDPNVqstSHo",
+  authDomain: "netflix-build-9975b.firebaseapp.com",
+  projectId: "netflix-build-9975b",
+  storageBucket: "netflix-build-9975b.appspot.com",
+  messagingSenderId: "443222286740",
+  appId: "1:443222286740:web:bc531899705abaa05e2f81",
+  measurementId: "G-6PR5L64W9H"
+};
