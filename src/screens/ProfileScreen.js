62b8904936be71a81775c10397eebba042c4c2d5
Vishnu S Mani <63743748+vishnutuchanathe@users.noreply.github.com>
2021-07-31 18:35:15 +0530

    add profile page

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -11,13 +11,12 @@ function ProfileScreen() {
         <div className="profileScreen">
             <Nav />
             <div className="profileScreen__body">
-                <h1>Edit Profile</h1>
+                <h1>Your Profile</h1>
                 <div className="profileScreen__info">
                     <img src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="" />
                     <div className="profileScreen__details">
                     <h2>{user.email}</h2>
                     <div className="profileScreen__plans">
-                    <h3>Plans</h3>
                         <button onClick={() => auth.signOut()} className="profileScreen__signOut">Sign Out</button>
                     </div>
                     </div>
